48612baeb55c473ec543c1a2c99280b628cd4197
Brendan Allen <99brendanallen@gmail.com>
2020-11-30 15:43:59 -0500

    Operators Tutorial w/ Exercise

---
+++ EXERCISE3_OPERATORS.sql
@@ -0,0 +1,6 @@
+USE sql_store;
+
+SELECT *
+FROM order_items
+WHERE (order_id = 6 AND unit_price * quantity > 30)
+
---
+++ mySQL_OPERATORS_OR.sql
@@ -0,0 +1,9 @@
+USE sql_store;
+
+SELECT * 
+FROM customers
+-- WHERE points > 3000
+-- WHERE state <> 'VA'
+WHERE birth_date > '1990-01-01' OR points > 1000
+
+
